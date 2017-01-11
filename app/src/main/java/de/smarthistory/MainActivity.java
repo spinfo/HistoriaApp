bc07634648079c7a82f7d0ddb52f156db496925a
David Neugebauer <david.neugebauer@posteo.de>
2017-01-11 11:50:57 +0100

    Removed files from example project.

--- app/src/main/java/de/smarthistory/MainActivity.java
+++ app/src/main/java/de/smarthistory/MainActivity.java
@@ -1,53 +1,298 @@
 package de.smarthistory;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.res.Resources;
+import android.os.Build;
+import android.preference.PreferenceManager;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.Toast;
 
+import org.osmdroid.api.IMapController;
+import org.osmdroid.config.Configuration;
+import org.osmdroid.tileprovider.modules.SqlTileWriter;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.Marker;
+import org.osmdroid.views.overlay.infowindow.MarkerInfoWindow;
+import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.logging.Logger;
+
+import de.smarthistory.data.DataFacade;
+import de.smarthistory.data.Mapstop;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final Logger LOGGER = Logger.getLogger(MainActivity.class.getName());
+    private  static final Logger LOGGER = Logger.getLogger(MainActivity.class.getName());
 
-    public final static String EXTRA_MESSAGE = "de.smarthistory.MESSAGE";
+    private DataFacade data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Request permissions to support Android Marshmallow and above devices
+        if (Build.VERSION.SDK_INT >= 23) {
+            checkPermissions();
+        }
+
+        //important! set your user agent to prevent getting banned from the osm servers
+        Configuration.getInstance().setUserAgentValue(BuildConfig.APPLICATION_ID);
+
+        // set up the data facade to use
+        this.data = DataFacade.getInstance();
+
+        // initialize the drawer menu
+        initializeNavDrawerMenu();
+
+        // set up the map to use
+        MapView map = (MapView) findViewById(R.id.map);
+        map.setTileSource(TileSourceFactory.MAPNIK);
+        map.setBuiltInZoomControls(true);
+        map.setMultiTouchControls(true);
+        IMapController mapController = map.getController();
+        mapController.setZoom(17);
+        GeoPoint startPoint = new GeoPoint(51.22049, 6.79202);
+        // GeoPoint startPoint = new GeoPoint(50.95863, 6.94487);
+        mapController.setCenter(startPoint);
+
+        // add Overlay for POIs
+        addPOIs(map);
+
+        // add Overlay for current location
+        addCurrentLocation(map);
     }
 
     /**
-     * Called when the user clicks the Send button
-     **/
-    public void sendMessage(View view) {
-        LOGGER.info("starting message activity");
-
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.edit_message);
-        String message = editText.getText().toString().replace("e", "ü");
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
-    }
-
-    /**
-     * Called when the user clicks the Map button
+     * refreshes the current osmdroid cache paths with user preferences plus soe logic to work around
+     * file system permissions on api23 devices. it's primarily used for out android tests.
+     * @param ctx
+     * @return current cache size in bytes
      */
-    public void switchToMap(View view) {
-        Intent intent = new Intent(this, MapViewActivity.class);
-        startActivity(intent);
+    public static long updateStoragePrefreneces(Context ctx){
+
+        //loads the osmdroid config from the shared preferences object.
+        //if this is the first time launching this app, all settings are set defaults with one exception,
+        //the tile cache. the default is the largest write storage partition, which could end up being
+        //this app's private storage, depending on device config and permissions
+
+        Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx));
+
+        //also note that our preference activity has the corresponding save method on the config object, but it can be called at any time.
+
+
+        File dbFile = new File(Configuration.getInstance().getOsmdroidTileCache().getAbsolutePath() + File.separator + SqlTileWriter.DATABASE_FILENAME);
+        if (Build.VERSION.SDK_INT >= 9 && dbFile.exists()) {
+            return dbFile.length();
+        }
+        return -1;
     }
+
 
     /**
-     * Called when the user clics the Fragments button
+     * gets storage state and current cache size
      */
-    public void switchToFragmentsExample(View view) {
-        LOGGER.info("starting frags activity");
+    private void updateStorageInfo(){
 
-        Intent intent = new Intent(this, FragmentsExampleActivity.class);
-        startActivity(intent);
+        long cacheSize = updateStoragePrefreneces(this);
+        //cache management ends here
+
+        // Original method cut of. Compare: https://github.com/osmdroid/osmdroid/blob/master/OpenStreetMapViewer/src/main/java/org/osmdroid/MainActivity.java#L168
+        // TODO: Check if we should include something like that here
     }
+
+
+    // START PERMISSION CHECK
+    final private int REQUEST_CODE_ASK_MULTIPLE_PERMISSIONS = 124;
+
+    private void checkPermissions() {
+        List<String> permissions = new ArrayList<>();
+        String message = "osmdroid permissions:";
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            permissions.add(Manifest.permission.ACCESS_FINE_LOCATION);
+            message += "\nLocation to show user location.";
+        }
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            permissions.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+            message += "\nStorage access to store map tiles.";
+        }
+        if (!permissions.isEmpty()) {
+            Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+            String[] params = permissions.toArray(new String[permissions.size()]);
+            // the if condition silences an android studio warning. Actual version check is done
+            // when calling checkPermissions()
+            if (Build.VERSION.SDK_INT >= 23) {
+                requestPermissions(params, REQUEST_CODE_ASK_MULTIPLE_PERMISSIONS);
+            }
+        } // else: We already have permissions, so handle as normal
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_CODE_ASK_MULTIPLE_PERMISSIONS: {
+                Map<String, Integer> perms = new HashMap<>();
+                // Initial
+                perms.put(Manifest.permission.ACCESS_FINE_LOCATION, PackageManager.PERMISSION_GRANTED);
+                perms.put(Manifest.permission.WRITE_EXTERNAL_STORAGE, PackageManager.PERMISSION_GRANTED);
+                // Fill with results
+                for (int i = 0; i < permissions.length; i++)
+                    perms.put(permissions[i], grantResults[i]);
+                // Check for ACCESS_FINE_LOCATION and WRITE_EXTERNAL_STORAGE
+                Boolean location = perms.get(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED;
+                Boolean storage = perms.get(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED;
+                if (location && storage) {
+                    // All Permissions Granted
+                    Toast.makeText(MainActivity.this, "All permissions granted", Toast.LENGTH_SHORT).show();
+                } else if (location) {
+                    Toast.makeText(this, "Storage permission is required to store map tiles to reduce data usage and for offline usage.", Toast.LENGTH_LONG).show();
+                } else if (storage) {
+                    Toast.makeText(this, "Location permission is required to show the user's location on map.", Toast.LENGTH_LONG).show();
+                } else { // !location && !storage case
+                    // Permission Denied
+                    Toast.makeText(MainActivity.this, "Storage permission is required to store map tiles to reduce data usage and for offline usage." +
+                            "\nLocation permission is required to show the user's location on map.", Toast.LENGTH_SHORT).show();
+                }
+                updateStorageInfo();
+            }
+            break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+    // END PERMISSION CHECK
+
+    // START MAP FUNCTIONS
+    private void addCurrentLocation(MapView map) {
+        GpsMyLocationProvider locationProvider = new GpsMyLocationProvider(this);
+        MyLocationNewOverlay myLocationOverlay = new MyLocationNewOverlay(locationProvider, map);
+
+        map.getOverlays().add(myLocationOverlay);
+        myLocationOverlay.enableMyLocation();
+    }
+
+    private void addPOIs(MapView map) {
+        List<Marker> markers = new ArrayList<>();
+
+        // custom info window for markers
+        MarkerInfoWindow window = new MapstopMarkerInfoWindow(R.layout.map_my_bonuspack_bubble, map);
+
+        for (Mapstop mapstop : data.getMapstops()) {
+            Marker marker = new Marker(map);
+            GeoPoint geoPoint = mapstop.getPlace().getLocation();
+            marker.setPosition(geoPoint);
+            marker.setTitle(mapstop.getPlace().getName());
+            marker.setRelatedObject(mapstop);
+
+            marker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
+            marker.setIcon(ContextCompat.getDrawable(this.getApplicationContext(), R.drawable.map_marker_icon_blue_small));
+
+            marker.setInfoWindow(window);
+
+            markers.add(marker);
+        }
+
+        map.getOverlays().addAll(markers);
+    }
+
+    // TODO extract to own file
+    private class MapstopMarkerInfoWindow extends MarkerInfoWindow {
+
+        private class MapstopMarkerInfoWindowOnclickListner implements View.OnClickListener {
+
+            Mapstop mapstop;
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, MapstopActivity.class);
+                intent.putExtra(getResources().getString(R.string.extra_key_mapstop), this.mapstop.getId());
+                startActivity(intent);
+            }
+
+            public void setMapstop(Mapstop mapstop) {
+                this.mapstop = mapstop;
+            }
+        };
+
+        MapstopMarkerInfoWindowOnclickListner onClickListener;
+
+        MapView map;
+
+        public MapstopMarkerInfoWindow(int layoutResId, final MapView mapView) {
+            super(layoutResId, mapView);
+            this.map = mapView;
+            this.onClickListener = new MapstopMarkerInfoWindowOnclickListner();
+        }
+
+        @Override
+        public void onOpen(Object item) {
+            super.onOpen(item);
+            closeAllInfoWindowsOn(this.map);
+
+            LinearLayout layout = (LinearLayout) getView().findViewById(R.id.map_my_bonuspack_bubble);
+            this.onClickListener.setMapstop((Mapstop) getMarkerReference().getRelatedObject());
+            layout.setClickable(true);
+            layout.setOnClickListener(this.onClickListener);
+        }
+    }
+
+    // END MAP FUNCTIONS
+
+    // START drawer menu
+    private void initializeNavDrawerMenu() {
+        final DrawerLayout mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        final ListView mDrawerList = (ListView) findViewById(R.id.left_drawer);
+
+        // set adapter for the list view
+        mDrawerList.setAdapter(new ArrayAdapter<String>(this, R.layout.drawer_list_item, getNavDrawerTitles()));
+
+        // a click listener, empty for now
+        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+    }
+
+    // The click listner for ListView in the navigation drawer
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            selectItem(position);
+        }
+    }
+
+    private String[] getNavDrawerTitles() {
+        final Resources res = getResources();
+        String[] result = {
+                res.getString(R.string.menu_current_area) + data.getCurrentArea().getName(),
+                res.getString(R.string.menu_current_tour) + data.getCurrentTour().getName(),
+                res.getString(R.string.menu_tours_available),
+                res.getString(R.string.menu_options_general)
+        };
+
+        return result;
+    }
+
+
+    // empty mehtod to be filled with code for when drawer Item is clicked
+    private void selectItem(int position) { }
+    // END drawer menu
+
 }
